2c8f84ea9b5d6d324c5ae98dce287f97b449f11a
HemanthKumar-veeville <hemanth.kumar@veeville.com>
2024-08-29 15:53:51 +0530

    Update Design

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,34 @@ import ProblemInput from "./ProblemInput";
 import Logo from "./assets/LifeWhispers.png";
 import Veeville from "./assets/Veeville.jpg";
 import IFL from "./assets/IFL.jpg";
+import BackgroundImage from "./assets/BG.png"; // Import your background image
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#FDF0E6] flex flex-col justify-center items-center">
-      <header className="text-center mb-8">
-        <div className="w-64 h-16">
+    <div className=" bg-[#FDF0E6] flex flex-col justify-center items-between">
+      <header className="flex flex-col items-center mb-8 mt-8">
+        <div className="w-64 h-20">
           <img src={Logo} alt="Life Whispers Logo" className="mx-auto" />
         </div>
-        <p className="text-gray-500">
-          Veeville. <span className="text-red-500">❤️</span> IndiaFirst Life
+        <p className="text-gray-500 w-fit text-center mr-11">
+          Veeville <span className="text-red-500">❤️</span> IndiaFirst Life
         </p>
       </header>
-      <ProblemInput />
+      <div
+        className="flex justify-center items-center"
+        style={{
+          backgroundImage: `url(${BackgroundImage})`,
+          backgroundSize: "100% 160%",
+          backgroundPosition: "center bottom",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <ProblemInput />
+      </div>
       <footer className="mt-16">
-        <div className="h-32 w-screen bg-white flex justify-around items-center">
-          <img src={IFL} alt="Footer Logo" className="h-10 w-40" />
-          <img src={Veeville} alt="Footer Logo" className="h-10 w-40" />
+        <div className="h-36 w-screen bg-white flex justify-around items-center">
+          <img src={IFL} alt="Footer Logo" className=" w-40" />
+          <img src={Veeville} alt="Footer Logo" className=" w-40" />
         </div>
       </footer>
     </div>
