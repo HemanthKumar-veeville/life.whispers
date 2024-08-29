b36cba54af8c0a7fc7ed933af324fa3d6d474ca8
HemanthKumar-veeville <hemanth.kumar@veeville.com>
2024-08-29 14:34:51 +0530

    Update UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ProblemInput from "./ProblemInput";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-[#F1F3F5] via-[#4BD0D1] to-[#1A7E82] flex flex-col justify-center items-center">
+    <div className="min-h-screen bg-gradient-to-b from-[#285A88] via-[#D1C5A4] to-[#ECE6DF] flex flex-col justify-center items-center">
       <header className="text-center mb-8">
         <h1 className="text-4xl font-bold text-white">LIFE WHISPERS</h1>
         <p className="text-white mt-2">Get advice as if from a family member</p>
